20c5558f084e10071fc5ece58274f980f62db7be
Vitor Duggen <zduggem@gmail.com>
2022-08-26 15:14:36 -0300

    Configuração jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  testMatch: ['**/src/**/?(*.)+(spec|test).[tj]s?(x)'],
+  moduleNameMapper: {
+    '^@atoms': '<rootDir>/src/app/shared/components/atoms/index',
+  },
+  setupFiles: ['<rootDir>/src/tests/unit/index.ts'],
 };
