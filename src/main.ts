fac8a18fef9cae52806fb675dd20f43e0aabea85
Vitor Duggen <zduggem@gmail.com>
2022-08-26 15:13:16 -0300

    Adicionado vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+
+import App from './app/app.vue';
+import router from './app/app-routes';
+import store from './app/app-state';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
