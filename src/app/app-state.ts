0b690f636a16a9fd6ffe0786afdaf63cbdf40f10
Vitor Duggen <zduggem@gmail.com>
2022-08-26 15:14:17 -0300

    Ajustado estrutura

--- src/app/app-state.ts
+++ src/app/app-state.ts
@@ -1,17 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import UserStore from './user/user-state';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    UserStore,
   },
 });
